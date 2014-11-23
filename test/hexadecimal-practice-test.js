3a66928b1e8fad397a3c10ebc0edd70f3922ef4e
Todd Wolfson <todd@twolfson.com>
2014-11-23 05:48:46 -0800

    Generating test utility

--- test/hexadecimal-practice-test.js
+++ test/hexadecimal-practice-test.js
@@ -1,9 +1,33 @@
 // Load in dependencies
+var assert = require('assert');
 var expect = require('chai').expect;
 var HexadecimalPractice = require('../');
 
 // Define test utilities
-var
+var testUtils = {
+  createSession: function () {
+    before(function createSessionFn () {
+      // Generate a practice session
+      var that = this;
+      this.questions = [];
+      this.practice = new HexadecimalPractice({
+        prompt: function (question, done) {
+          // When a question is asked, save it
+          that.questions.push(questions);
+
+          // Generate an answer
+          assert(that.responder, 'Expected `that.responder` to exist for `prompt`. Please define it to answer questions');
+          var answer = that.responder(question);
+
+          // Respond with the answer
+          process.nextTick(function sendAnswer () {
+            done(null, answer);
+          });
+        }
+      });
+    });
+  }
+};
 
 // Start our tests
 describe('A hexadecimal practice session', function () {
