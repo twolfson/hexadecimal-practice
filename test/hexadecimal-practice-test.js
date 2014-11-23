42e007c61897f140fc330ed4f2d2dfef5779903b
Todd Wolfson <todd@twolfson.com>
2014-11-23 05:43:17 -0800

    Building out our test suite

--- test/hexadecimal-practice-test.js
+++ test/hexadecimal-practice-test.js
@@ -1,10 +1,37 @@
 // Load in dependencies
-var assert = require('assert');
-var hexadecimalPractice = require('../');
+var expect = require('chai').expect;
+var HexadecimalPractice = require('../');
+
+// Define test utilities
+var
 
 // Start our tests
-describe('hexadecimal-practice', function () {
-  it('returns awesome', function () {
-    assert.strictEqual(hexadecimalPractice(), 'awesome');
+describe('A hexadecimal practice session', function () {
+  describe('when prompting for a set of numbers', function () {
+    describe('when a valid answer is provided', function () {
+      it('calls back', function () {
+
+      });
+    });
+
+    describe.skip('when an invalid answer is provided', function () {
+      it('prompts again', function () {
+
+      });
+    });
+  });
+});
+
+describe.skip('A CLI practice session', function () {
+  it('prompts the user', function () {
+
+  });
+
+  it('allows the user to respond', function () {
+
+  });
+
+  it('can be terminated', function () {
+    // Ctrl+C
   });
 });
